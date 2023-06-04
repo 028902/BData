0e39b39c5803760b80df19955ba5e8256bdcceeb
028902 <conscience9812@gmail.com>
2023-06-04 21:13:14 +0900

    Initial commit

---
+++ src/main/java/com/example/bdata/BDataApplication.java
@@ -0,0 +1,13 @@
+package com.example.bdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BDataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bdata/BDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
